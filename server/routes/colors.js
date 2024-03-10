561d94644f21dfcd33d9b3c7627781271508bef0
Victor Alcaraz <vctralcaraz@gmail.com>
2024-03-09 23:58:41 -0800

    bonus done

--- server/routes/colors.js
+++ server/routes/colors.js
@@ -8,7 +8,15 @@ router.get('/', (req, res) => {
 
 router.get('/:name', (req, res) => {
     res.json('GET /colors/:name');
-})
+});
+
+router.post('/:name/css-styles', (req, res) => {
+    res.json('GET /colors/:name/css-styles');
+});
+
+router.delete('/:name/css-styles/:style', (req, res) => {
+    res.json('DELETE /colors/:name/css-styles/:style');
+});
 
 
 module.exports = router;
